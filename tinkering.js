93b8be80206dc307ad36d02ec5d7660df00c9b04
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-03 05:44:28 +0000

    Tinkering of Javascript code

---
+++ tinkering.js
@@ -0,0 +1,45 @@
+/*function multiply(a, b) {
+  return a * b;
+}
+const result = multiply(2, 4);
+console.log(result);*/
+
+//Converting Temperature
+/*var celcius = 12;
+var farenheit = (celcius * 1.8) + 32;
+console.log(farenheit);*/
+
+//console.log("Up up\n\tDown down");
+
+/*// Pick a string. Your string can have any number of characters.
+var my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0.
+var ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);*/
+
+//Check null and undefined
+/*var signedIn;
+console.log(signedIn);
+
+var signedIn = null;
+console.log(signedIn);*/
+
+//Quiz quest in Udacity.com
+/*var thingOne = "red";
+var thingTwo = "blue";
+
+//
+var bill = 10.25 + 3.99 + 7.15;
+
+var tip = bill * 0.15;
+
+var total = bill + tip;
+
+console.log(total.toFixed(2));
+
+
+
+console.log(thingOne + thingTwo);*/
