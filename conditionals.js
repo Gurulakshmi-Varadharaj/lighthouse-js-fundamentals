47c2df702e525bdb39ba34d152973def7e498b3c
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-03 05:26:46 +0000

    True and false condition with default statement

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,19 @@
 const raining = true;
 
+/*if (raining) {
+  console.log("Don't forget your umbrella!");
+}*/
+
+const cold = false;
+
 if (raining) {
   console.log("Don't forget your umbrella!");
 }
+
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
 
